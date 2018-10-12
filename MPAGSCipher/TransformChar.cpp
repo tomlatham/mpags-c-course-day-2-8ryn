e8958fe0a75b835fbf742b1d2558281b0af64748
Bryn Roberts <bryn@BrynsPC.localdomain>
2018-10-12 15:30:41 +0100

    Partial implementation of separation

---
+++ MPAGSCipher/TransformChar.cpp
@@ -0,0 +1,41 @@
+#include <string>
+
+#include <cctype>
+
+std::string transformChar( const char in_char){
+  /*Capitalises and returns any alphabetic characters and transliterates digits to English words
+    and returns them capitalised*/
+
+  // Uppercase alphabetic characters
+  if (std::isalpha(in_char)) {
+    return std::string(1,std::toupper(in_char));
+  }
+
+  // Transliterate digits to English words
+  switch (in_char) {
+  case '0':
+    return "ZERO";
+  case '1':
+    return "ONE";
+  case '2':
+    return "TWO";
+  case '3':
+    return "THREE";
+  case '4':
+    return "FOUR";
+  case '5':
+    return "FIVE";
+  case '6':
+    return "SIX";
+  case '7':
+    return "SEVEN";
+  case '8':
+    return "EIGHT";
+  case '9':
+    return "NINE";
+  }
+
+  // If the character isn't alphabetic or numeric, DONT add it.
+  // Our ciphers can only operate on alphabetic characters.
+  return "";
+}
--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -6,45 +6,99 @@
 // For std::isalpha and std::isupper
 #include <cctype>
 
-//TODO: Insert comment explaining purpose
-std::string transformChar( const char in_char){
+// Transliterate char to string
+std::string transformChar( const char in_char);
 
-  // Uppercase alphabetic characters
-  if (std::isalpha(in_char)) {
-    return std::string(1,std::toupper(in_char));
+// Process the comman line arguments
+bool processCommandLine( const std::vector<std::string>& args,
+			 bool& helpRequested, bool& versionRequested, std::string& inputFileName, std::string& outputFileName );
+
+// Main function of the mpags-cipher program
+  int main(int argc, char* argv[])
+{
+  // Convert the command-line arguments into a more easily usable form
+  const std::vector<std::string> cmdLineArgs {argv, argv+argc};
+
+  // Options that might be set by the command-line arguments
+  bool helpRequested {false};
+  bool versionRequested {false};
+  std::string inputFile {""};
+  std::string outputFile {""};
+
+  if(!processCommandLine(cmdLineArgs, helpRequested, versionRequested, inputFile, outputFile)){
+    return 1;
   }
 
-  // Transliterate digits to English words
-  switch (in_char) {
-  case '0':
-    return "ZERO";
-  case '1':
-    return "ONE";
-  case '2':
-    return "TWO";
-  case '3':
-    return "THREE";
-  case '4':
-    return "FOUR";
-  case '5':
-    return "FIVE";
-  case '6':
-    return "SIX";
-  case '7':
-    return "SEVEN";
-  case '8':
-    return "EIGHT";
-  case '9':
-    return "NINE";
+  // Handle help, if requested
+  if (helpRequested) {
+    // Line splitting for readability
+    std::cout
+      << "Usage: mpags-cipher [-i <file>] [-o <file>]\n\n"
+      << "Encrypts/Decrypts input alphanumeric text using classical ciphers\n\n"
+      << "Available options:\n\n"
+      << "  -h|--help        Print this help message and exit\n\n"
+      << "  --version        Print version information\n\n"
+      << "  -i FILE          Read text to be processed from FILE\n"
+      << "                   Stdin will be used if not supplied\n\n"
+      << "  -o FILE          Write processed text to FILE\n"
+      << "                   Stdout will be used if not supplied\n\n";
+    // Help requires no further action, so return from main
+    // with 0 used to indicate success
+    return 0;
+  }
+  
+
+
+  // Handle version, if requested
+  // Like help, requires no further action,
+  // so return from main with zero to indicate success
+  if (versionRequested) {
+    std::cout << "0.1.0" << std::endl;
+    return 0;
   }
 
-  // If the character isn't alphabetic or numeric, DONT add it.
-  // Our ciphers can only operate on alphabetic characters.
+  // Initialise variables for processing input text
+  char inputChar {'x'};
+  std::string inputText {""};
+
+  // Read in user input from stdin/file
+  // Warn that input file option not yet implemented
+  if (!inputFile.empty()) {
+    std::cout << "[warning] input from file ('"
+	      << inputFile
+	      << "') not implemented yet, using stdin\n";
+  }
+
+  // Loop over each character from user input
+  // (until Return then CTRL-D (EOF) pressed)
+  while(std::cin >> inputChar)
+    {
+      inputText += transformChar(inputChar);
+    }
+
+
+  // Output the transliterated text
+  // Warn that output file option not yet implemented
+  if (!outputFile.empty()) {
+    std::cout << "[warning] output to file ('"
+	      << outputFile
+	      << "') not implemented yet, using stdout\n";
+  }
+
+  std::cout << inputText << std::endl;
+
+  // No requirement to return from main, but we do so for clarity
+  // and for consistency with other functions
+  return 0;
 }
 
 bool processCommandLine( const std::vector<std::string>& args,
 			 bool& helpRequested, bool& versionRequested, std::string& inputFileName,
 			 std::string& outputFileName ){
+  /* Takes arguments and parses them to find if help or version number are requested.
+     Sets the input and output strings if provided.*/
+  
+  
   // Add a typedef that assigns another name for the given type for clarity
   typedef std::vector<std::string>::size_type size_type;
   const size_type nArgs {args.size()};
@@ -94,82 +148,4 @@ bool processCommandLine( const std::vector<std::string>& args,
     }
   }
   return true;
-  
 }
-
-// Main function of the mpags-cipher program
-int main(int argc, char* argv[])
-{
-  // Convert the command-line arguments into a more easily usable form
-  const std::vector<std::string> cmdLineArgs {argv, argv+argc};
-
-  // Options that might be set by the command-line arguments
-  bool helpRequested {false};
-  bool versionRequested {false};
-  std::string inputFile {""};
-  std::string outputFile {""};
-
-  if(!processCommandLine(cmdLineArgs, helpRequested, versionRequested, inputFile, outputFile)){
-      return 1;
-    }
-
-    // Handle help, if requested
-    if (helpRequested) {
-      // Line splitting for readability
-      std::cout
-	<< "Usage: mpags-cipher [-i <file>] [-o <file>]\n\n"
-	<< "Encrypts/Decrypts input alphanumeric text using classical ciphers\n\n"
-	<< "Available options:\n\n"
-	<< "  -h|--help        Print this help message and exit\n\n"
-	<< "  --version        Print version information\n\n"
-	<< "  -i FILE          Read text to be processed from FILE\n"
-	<< "                   Stdin will be used if not supplied\n\n"
-	<< "  -o FILE          Write processed text to FILE\n"
-	<< "                   Stdout will be used if not supplied\n\n";
-      // Help requires no further action, so return from main
-      // with 0 used to indicate success
-      return 0;
-    }
-
-    // Handle version, if requested
-    // Like help, requires no further action,
-    // so return from main with zero to indicate success
-    if (versionRequested) {
-      std::cout << "0.1.0" << std::endl;
-      return 0;
-    }
-
-    // Initialise variables for processing input text
-    char inputChar {'x'};
-    std::string inputText {""};
-
-    // Read in user input from stdin/file
-    // Warn that input file option not yet implemented
-    if (!inputFile.empty()) {
-      std::cout << "[warning] input from file ('"
-		<< inputFile
-		<< "') not implemented yet, using stdin\n";
-    }
-
-    // Loop over each character from user input
-    // (until Return then CTRL-D (EOF) pressed)
-    while(std::cin >> inputChar)
-      {
-	inputText += transformChar(inputChar);
-      }
-
-
-    // Output the transliterated text
-    // Warn that output file option not yet implemented
-    if (!outputFile.empty()) {
-      std::cout << "[warning] output to file ('"
-		<< outputFile
-		<< "') not implemented yet, using stdout\n";
-    }
-
-    std::cout << inputText << std::endl;
-
-    // No requirement to return from main, but we do so for clarity
-    // and for consistency with other functions
-    return 0;
-    }
